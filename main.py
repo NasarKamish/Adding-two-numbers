bdf7a1dd1a57bcedb9f1533f7ca81a0cbfc40fe1
NasarKamish <Nasarkamish@gmail.com>
2021-05-06 09:48:59 +0200

    modified

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ Addition.title("Adding two numbers")
 
 
 def add():
-    my_sum = str(float(en_First.get()) + float(en_Second.get()))
+    my_sum = str(int(en_First.get()) + int(en_Second.get()))
+    en_Answer.config(state="normal")
+    en_Answer.delete(0, END)
+    en_Answer.insert(0, my_sum)
+    en_Answer.config(state="readonly")
     addition.set(my_sum)
 
 
 def clear():
     en_First.delete(0, END)
     en_Second.delete(0, END)
+    en_Answer.delete(0, END)
 
 
 addition = StringVar()
@@ -26,8 +31,9 @@ en_Second = Entry(Addition)
 en_Second.place(x=210, y=30)
 lbl_Answer = Label(Addition, text="Your answer: ")
 lbl_Answer.place(x=0, y=60)
-lbl_Answer2 = Label(Addition, textvariable=addition)
-lbl_Answer2.place(x=210, y=60)
+en_Answer = Entry(Addition)
+en_Answer.config(state="readonly")
+en_Answer.place(x=210, y=60)
 btn_Add = Button(Addition, text="Add", command=add)
 btn_Clear = Button(Addition, text="Clear", command=clear)
 btn_Exit = Button(Addition, text="Exit", command=exit)
